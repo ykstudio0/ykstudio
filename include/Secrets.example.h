616f5bbdfc177ffdc8a205029629c90fe1254e87
ykstudio0 <ykstudio@netsgo.com>
2026-08-03 13:36:39 +0900

    SVEMS v0.6.0 WiFi Service

--- include/Secrets.example.h
+++ include/Secrets.example.h
@@ -11,9 +11,23 @@
 
 namespace SVEMS::Config
 {
+    // Wi-Fi
     inline constexpr char WIFI_SSID[] =
         "YOUR_WIFI_SSID";
 
     inline constexpr char WIFI_PASSWORD[] =
         "YOUR_WIFI_PASSWORD";
+
+    // MQTT (향후)
+    inline constexpr char MQTT_SERVER[] =
+        "";
+
+    inline constexpr uint16_t MQTT_PORT =
+        1883;
+
+    inline constexpr char MQTT_USER[] =
+        "";
+
+    inline constexpr char MQTT_PASSWORD[] =
+        "";
 }
